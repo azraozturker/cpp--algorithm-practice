ac15b88477d83595d2b8f03b9ae6093dd4ba7df8
azraozturker <ozturkerazra@gmail.com>
2025-12-02 11:57:44 +0300

    Implement perfect number checker in main.cpp

---
+++ perfect-number/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num, sum = 0;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    for (int i = 1; i <= num / 2; i++) {
+        if (num % i == 0) {
+            sum += i;
+        }
+    }
+
+    if (sum == num) {
+        cout << num << " is a perfect number.";
+    } else {
+        cout << num << " is not a perfect number.";
+    }
+
+    return 0;
+}
